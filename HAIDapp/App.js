f917d727df23240bc40bbfe0e96f5f04dc13bf33
Retsek0 <kestergriffiths@googlemail.com>
2022-10-27 00:25:25 +0100

    Routing for signup, login and home pages established. Temporary components.

--- HAIDapp/App.js
+++ HAIDapp/App.js
@@ -1,13 +1,22 @@
 // App created by Kester,
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from "./Home/Home.js";
+import Login from "./Login/Login.js";
+import SignUp from './SignUp/SignUp.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="home" component={Home}/>
+        <Stack.Screen name="login" component={Login}/>
+        <Stack.Screen name="signup" component={SignUp}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 

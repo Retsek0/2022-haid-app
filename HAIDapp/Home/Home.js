5e05c090c73266a0627f07b67fde8e5e3a62b3d8
Retsek0 <kestergriffiths@googlemail.com>
2022-10-27 16:11:17 +0100

    Divided home screen into 3 views

--- HAIDapp/Home/Home.js
+++ HAIDapp/Home/Home.js
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 
-export default function Home() {
+export default function Home(props) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+        }}>
+            <View style={{height: 50, backgroundColor: 'powderblue'}} /*This will be a status bar with a greeting and a button to access your profile/settings*/ /> 
+            <View style={{flex:1, backgroundColor: 'skyblue'}} /*This will be a NavigationContainer containing the explore, saved and bookings which are scrollable views*/ />
+            <View style={{height: 50, backgroundColor: 'steelblue'}} /*This will be the activity menu, a row of buttons allowing the user to switch between explore, saved and bookings*/ />
+        </View>
     );
-  }
+}

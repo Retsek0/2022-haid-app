324c52fa35e4d0ee40ee836985d1980dd0f96379
Vincent Zhenhao ZHAO <73338131+Vincent-Zhenhao-ZHAO@users.noreply.github.com>
2022-10-28 12:40:53 +0100

    Update Home.js

--- HAIDapp/Home/Home.js
+++ HAIDapp/Home/Home.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Home() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Vincent screen</Text>
+        <Text>Home screen</Text>
       </View>
     );
   }

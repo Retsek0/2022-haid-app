9778cc3ea5d3b78b715c6a95efc16e7d70923930
Vincent Zhenhao ZHAO <73338131+Vincent-Zhenhao-ZHAO@users.noreply.github.com>
2022-10-28 12:41:37 +0100

    Update Login.js
    Add comments.

--- HAIDapp/Login/Login.js
+++ HAIDapp/Login/Login.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+// Vincent
 export default function Login() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>

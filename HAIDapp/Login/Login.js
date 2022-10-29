39746de5c1478c59d8c488b9c1082a170d889fdc
Zhenhao Zhao <cvhm34@durham.ac.uk>
2022-10-29 22:39:17 +0100

    First version of Login-Page
    - Have done initial stage of Login page
    - But theres something need to improve:
       a: For login box need to match the design
       b: we might need a line about 'I understand the policy' to build trust with user (mentioned in the lecture)
       c: connecting with the other pages.

--- HAIDapp/Login/Login.js
+++ HAIDapp/Login/Login.js
@@ -1,9 +1,88 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {KeyboardAvoidingView , StyleSheet, Text, View, Button, TextInput, Image} from 'react-native';
 
+// Vincent
 export default function Login() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Login</Text>
+  return (
+    <KeyboardAvoidingView 
+    style={styles.container}
+    behavior="padding"
+    >
+
+      {/* Background picture */}
+      <View>
+        <Image source={require('./assets/TraveleurLogo.png')} />
       </View>
-    );
+
+      {/* Login box */}
+      <View style = {styles.inputContainer}  >
+          <TextInput style = {styles.textInput} placeholder='Your Email' />
+          <TextInput style = {styles.textInput} placeholder='Your Password' secureTextEntry />
+      </View>
+
+      {/* Login */}
+      <View style = { styles.loginBtn }>
+        <Button title='LOGIN' style={styles.btn} />
+      </View>
+
+      {/* Dont have account */}
+      <View style = { styles.lastWords}>
+        <Text> Don't have an account?</Text>
+        <Text> Sign Up </Text>
+      </View>
+
+      <StatusBar style="auto" />
+
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    backgroundColor: "#F8F9FA",
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  Image: {
+    maxHeight: 200,
+    marginTop: 20,
+  },
+
+  inputContainer: {
+    borderRadius: 30,
+    width: "60%",
+    height: 60,
+    marginBottom: 5,
+    alignItems: "center",
+    justifyContent: 'center',
+  },
+
+  textInput: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+
+  loginBtn: {
+    backgroundColor: "#3F6A84" ,
+    width: "30%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 25,
+  },
+
+  btn: {
+    fontSize: 20,
+    color: "#F8F9FA",
+  },
+
+  lastWords: {
+    flexDirection: 'row',
+    marginTop: 40,
   }
+
+});

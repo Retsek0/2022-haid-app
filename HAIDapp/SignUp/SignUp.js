4b9bc38a9684bb93d201d2013227da9eaeffb7a6
Zhenhao Zhao <cvhm34@durham.ac.uk>
2022-10-30 22:48:50 +0000

    Initial SignUp finished

--- HAIDapp/SignUp/SignUp.js
+++ HAIDapp/SignUp/SignUp.js
@@ -1,13 +1,143 @@
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {KeyboardAvoidingView , StyleSheet, Text, View, Button, TextInput, Image, Pressable} from 'react-native';
+import Icon from '@expo/vector-icons/AntDesign';
+
+// Vincent
+// Need to add comfirm button (like you have create new account)
 
 export default function SignUp(props) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Sign Up</Text>
-        <Button
-            title="Already have an account?"
-            onPress={() => props.navigation.navigate("Login")}
-        />
+  return (
+    <KeyboardAvoidingView 
+    style={styles.container}
+    behavior="padding"
+    >
+  
+      <Text style={styles.beginning}> CREATE ACCOUNT </Text>
+      {/* Full name box */}
+      <View style = {styles.inputContainer}>
+          <View paddingHorizontal= {10} >
+            <Icon name="user" color={"#00716F"} size={25} />
+          </View>
+          
+          <TextInput style = {styles.textInput} placeholder='FULL NAME' />
       </View>
-    );
+  
+      <View style = {styles.inputContainer}>
+        <View paddingHorizontal= {10} >
+            <Icon name="mail" color={"#00716F"} size={25} />
+          </View>
+          <TextInput style = {styles.textInput} placeholder='YOUR EMAIL'/>
+      </View>
+
+      <View style = {styles.inputContainer}>
+        <View paddingHorizontal= {10} >
+            <Icon name="lock" color={"#00716F"} size={25} />
+          </View>
+          <TextInput style = {styles.textInput} placeholder='Password' secureTextEntry />
+      </View>
+
+      <View style = {styles.inputContainer}>
+        <View paddingHorizontal= {10} >
+            <Icon name="lock" color={"#00716F"} size={25} />
+          </View>
+          <TextInput style = {styles.textInput} placeholder='Confirm Password' secureTextEntry />
+      </View>
+      
+  
+      {/* SignUp */}
+      <View style = {styles.signBtn}>
+  
+        <Pressable onPress={() => props.navigation.navigate("Login")}>
+          <Text style= {styles.signtext}> SignUp </Text>
+        </Pressable>
+  
+      </View>
+  
+      {/* Already have account */}
+      <View style = { styles.lastWords}>
+        <Text> Already have an account? </Text>
+        <Pressable onPress={() => props.navigation.navigate("Login")}>
+          <Text
+            style={styles.underline}
+          > 
+            Sign in
+          </Text>
+        </Pressable>
+        
+      </View>
+  
+      <StatusBar style="auto" />
+  
+    </KeyboardAvoidingView>
+  );
   }
+  
+  const styles = StyleSheet.create({
+  
+// need to put it at left
+  beginning: {
+    fontSize: 15,
+    marginHorizontal: 20,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    color: "#3F6A84"
+  },
+
+  container: {
+    backgroundColor: "#F8F9FA",
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  Image: {
+    maxHeight: 200,
+    marginTop: 20,
+  },
+  
+  inputContainer: {
+    flexDirection: "row",
+    borderRadius: 10,
+    borderWidth: 2,
+    width: "60%",
+    height: 60,
+    marginTop: 30,
+    marginHorizontal: 40,
+    alignItems: 'center',
+  },
+  
+  signBtn: {
+    marginHorizontal: 55,
+    alignItems: "center",
+    justifyContent: 'center',
+    marginTop: 30,
+    backgroundColor: '#3F6A84',
+    paddingVertical: 8,
+    width: "30%",
+    borderRadius: 23,
+  },
+  
+  // need to add font
+  signtext: {
+    color: "#F8F9FA",
+  },
+  
+  // need to be more smooth
+  textInput: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  
+  lastWords: {
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  
+  underline: {
+    textDecorationLine: 'underline',
+    color: "#3F6A84"
+  }
+  
+  });
+  
+  

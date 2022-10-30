77d2f206511439767bdad88725712cdd827e3dba
Retsek0 <kestergriffiths@googlemail.com>
2022-10-30 23:55:10 +0000

    Added ScrollView to Explore.js

--- HAIDapp/Explore/Explore.js
+++ HAIDapp/Explore/Explore.js
@@ -1,9 +1,28 @@
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, StyleSheet } from 'react-native';
 
 export default function Explore(props) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Explore page</Text>
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+        }}>
+            <View style={{height: 50, backgroundColor: 'powderblue'}}/>
+            <ScrollView /* Each listing hardcoded for now, but in future should be mapped from an array into a series of <Listing> components */>
+                <View style={styles.listing}><Text>1</Text></View>
+                <View style={styles.listing}><Text>2</Text></View>
+                <View style={styles.listing}><Text>3</Text></View>
+                <View style={styles.listing}><Text>4</Text></View>
+                <View style={styles.listing}><Text>5</Text></View>
+            </ScrollView>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    listing: {
+        backgroundColor: "red",
+        width: "90%"
+    }
+})

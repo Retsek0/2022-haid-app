5871478493f0e21369b213d79cbda5d142e2e715
Zhenhao Zhao <cvhm34@durham.ac.uk>
2022-10-30 22:41:06 +0000

    Add new features in Login

--- HAIDapp/Login/Login.js
+++ HAIDapp/Login/Login.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import {KeyboardAvoidingView , StyleSheet, Text, View, Button, TextInput, Image} from 'react-native';
+import {KeyboardAvoidingView , StyleSheet, Text, View, Button, TextInput, Image, Pressable} from 'react-native';
+import Icon from '@expo/vector-icons/AntDesign';
 
 // Vincent
 
@@ -16,26 +17,42 @@ return (
     </View>
 
     {/* Login box */}
-    <View style = {styles.inputContainer}  >
+    <View style = {styles.inputContainer}>
+        <View paddingHorizontal= {10} >
+          <Icon name="mail" color={"#00716F"} size={25} />
+        </View>
+        
         <TextInput style = {styles.textInput} placeholder='Your Email' />
-        <TextInput style = {styles.textInput} placeholder='Your Password' secureTextEntry />
     </View>
 
+    <View style = {styles.inputContainer}>
+      <View paddingHorizontal= {10} >
+          <Icon name="lock" color={"#00716F"} size={25} />
+        </View>
+        <TextInput style = {styles.textInput} placeholder='Your Password' secureTextEntry />
+    </View>
+   
+
     {/* Login */}
-    <View style = { styles.loginBtn }>
-      <Button title='LOGIN' style={styles.btn} onPress={() => props.navigation.navigate("Home")}/>
+    <View style = {styles.loginBtn}>
+
+      <Pressable onPress={() => props.navigation.navigate("Home")}>
+        <Text style= {styles.logintext}> Login </Text>
+      </Pressable>
+
     </View>
 
     {/* Dont have account */}
     <View style = { styles.lastWords}>
       <Text> Don't have an account? </Text>
-      {/* this later will be functioned as a button.  */}
-      <Text
-        style={styles.underline}
-        onPress={() => props.navigation.navigate("SignUp")}
-      > 
-        Sign Up 
-      </Text>
+      <Pressable onPress={() => props.navigation.navigate("SignUp")}>
+        <Text
+          style={styles.underline}
+        > 
+          Sign Up 
+        </Text>
+      </Pressable>
+      
     </View>
 
     <StatusBar style="auto" />
@@ -59,32 +76,36 @@ Image: {
 },
 
 inputContainer: {
-  borderRadius: 30,
+  flexDirection: "row",
+  borderRadius: 10,
+  borderWidth: 2,
   width: "60%",
-  height: 60,
-  marginBottom: 5,
-  alignItems: "center",
-  justifyContent: 'center',
-},
-
-textInput: {
-  paddingHorizontal: 15,
-  paddingVertical: 10,
+  height: 50,
+  marginTop: 20,
+  marginHorizontal: 40,
+  alignItems: 'center',
 },
 
 loginBtn: {
-  backgroundColor: "#3F6A84" ,
-  width: "30%",
-  borderRadius: 25,
-  height: 50,
+  marginHorizontal: 55,
   alignItems: "center",
-  justifyContent: "center",
-  marginTop: 25,
+  justifyContent: 'center',
+  marginTop: 20,
+  backgroundColor: '#3F6A84',
+  paddingVertical: 8,
+  width: "30%",
+  borderRadius: 23,
 },
 
-btn: {
-  fontSize: 20,
+// need to add font
+logintext: {
   color: "#F8F9FA",
+},
+
+// need to be more smooth
+textInput: {
+  paddingHorizontal: 15,
+  paddingVertical: 10,
 },
 
 lastWords: {
@@ -93,7 +114,8 @@ lastWords: {
 },
 
 underline: {
-  textDecorationLine: 'underline'
+  textDecorationLine: 'underline',
+  color: "#3F6A84"
 }
 
 });

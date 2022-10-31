b3bd13823588bc99bfdb5fde2945f0e7bc73e3ab
Wenbo-1118 <73350512+Wenbo-1118@users.noreply.github.com>
2022-10-31 13:50:02 +0000

    Add files via upload

--- HAIDapp/App.js
+++ HAIDapp/App.js
@@ -1,29 +1,10 @@
-import { StyleSheet} from 'react-native';
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from "./Home/Home.js";
-import Login from "./Login/Login.js";
-import SignUp from './SignUp/SignUp.js';
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
+function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="SignUp" component={SignUp}/>
-        <Stack.Screen name="Home" component={Home}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MainContainer/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

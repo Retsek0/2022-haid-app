82c3607fc1c9bb43ac741ecb05abb7d7c944fd56
Retsek0 <kestergriffiths@googlemail.com>
2022-10-31 17:46:26 +0000

    Center aligned items

--- HAIDapp/Explore/Explore.js
+++ HAIDapp/Explore/Explore.js
@@ -1,28 +1,86 @@
-import { Text, View, ScrollView, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Pressable } from 'react-native';
 
 export default function Explore(props) {
     return (
-        <View style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-        }}>
+        <View style={styles.container}>
             <View style={{height: 50, backgroundColor: 'powderblue'}}/>
-            <ScrollView /* Each listing hardcoded for now, but in future should be mapped from an array into a series of <Listing> components */>
-                <View style={styles.listing}><Text>1</Text></View>
-                <View style={styles.listing}><Text>2</Text></View>
-                <View style={styles.listing}><Text>3</Text></View>
-                <View style={styles.listing}><Text>4</Text></View>
-                <View style={styles.listing}><Text>5</Text></View>
+            <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{
+                    justifyContent: 'center', alignItems: 'center',
+                    backgroundColor: '#fff',
+                    borderRadius: 25,
+                    padding: 20
+                }}>
+                    <Pressable style={styles.listing}>
+                        <Text>Test</Text>
+                    </Pressable>
+                    <Pressable style={styles.listing}>
+                        <Text>Test</Text>
+                    </Pressable>
+                    <Pressable style={styles.listing}>
+                        <Text>Test</Text>
+                    </Pressable>
+                    <Pressable style={styles.listing}>
+                        <Text>Test</Text>
+                    </Pressable>
+                </View>
+            </ScrollView>
+        </View>
+    );
+}
+    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1'
+    },
+
+    listing: {
+        alignItems: "center",
+        justifyContent: 'center',
+        marginTop: 20,
+        backgroundColor: '#3F6A84',
+        paddingVertical: 8,
+        width: "30%",
+        borderRadius: 23,
+    },
+});
+
+{/*    return (
+        <View>
+            <View style={{height: 50, backgroundColor: 'powderblue'}}/>
+            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', borderColor: 'red', borderWidth: 2 }}>
+                <Pressable style={styles.listing}>
+                    <Text>Test</Text>
+                </Pressable>
+                <Pressable style={styles.listing}>
+                    <Text>Test</Text>
+                </Pressable>
+                <Pressable style={styles.listing}>
+                    <Text>Test</Text>
+                </Pressable>
+                <Pressable style={styles.listing}>
+                    <Text>Test</Text>
+                </Pressable>
             </ScrollView>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    listing: {
+    list: {
         backgroundColor: "red",
-        width: "90%"
-    }
-})
+        alignItems: "center"
+    },
+    
+    listing: {
+        alignItems: "center",
+        justifyContent: 'center',
+        marginTop: 20,
+        backgroundColor: '#3F6A84',
+        paddingVertical: 8,
+        width: "30%",
+        borderRadius: 23,
+    },
+})*/}

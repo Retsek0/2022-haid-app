a184ee044065b39c5c093d157198c0f00da54cd2
Retsek0 <kestergriffiths@googlemail.com>
2022-10-31 21:02:34 +0000

    Working on the ability to pass image as prop to Listing component

--- HAIDapp/Explore/Explore.js
+++ HAIDapp/Explore/Explore.js
@@ -1,28 +1,16 @@
-import { Text, View, ScrollView, StyleSheet, Pressable } from 'react-native';
+import { View, ScrollView, StyleSheet, Pressable } from 'react-native';
+import Listing from './Listing';
+import { listings } from '../assets/listings';
 
 export default function Explore(props) {
     return (
         <View style={styles.container}>
             <View style={{height: 50, backgroundColor: 'powderblue'}}/>
-            <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{
-                    justifyContent: 'center', alignItems: 'center',
-                    backgroundColor: '#fff',
-                    borderRadius: 25,
-                    padding: 20
-                }}>
-                    <Pressable style={styles.listing}>
-                        <Text>Test</Text>
-                    </Pressable>
-                    <Pressable style={styles.listing}>
-                        <Text>Test</Text>
-                    </Pressable>
-                    <Pressable style={styles.listing}>
-                        <Text>Test</Text>
-                    </Pressable>
-                    <Pressable style={styles.listing}>
-                        <Text>Test</Text>
-                    </Pressable>
+            <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+                <View>
+                    {listings.map((listing, i) => {
+                        return <Listing key={i} imageBackground={listing.imageBackground}/>
+                    })}
                 </View>
             </ScrollView>
         </View>
@@ -46,41 +34,3 @@ const styles = StyleSheet.create({
         borderRadius: 23,
     },
 });
-
-{/*    return (
-        <View>
-            <View style={{height: 50, backgroundColor: 'powderblue'}}/>
-            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', borderColor: 'red', borderWidth: 2 }}>
-                <Pressable style={styles.listing}>
-                    <Text>Test</Text>
-                </Pressable>
-                <Pressable style={styles.listing}>
-                    <Text>Test</Text>
-                </Pressable>
-                <Pressable style={styles.listing}>
-                    <Text>Test</Text>
-                </Pressable>
-                <Pressable style={styles.listing}>
-                    <Text>Test</Text>
-                </Pressable>
-            </ScrollView>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-    list: {
-        backgroundColor: "red",
-        alignItems: "center"
-    },
-    
-    listing: {
-        alignItems: "center",
-        justifyContent: 'center',
-        marginTop: 20,
-        backgroundColor: '#3F6A84',
-        paddingVertical: 8,
-        width: "30%",
-        borderRadius: 23,
-    },
-})*/}
---
+++ HAIDapp/assets/listings.js
@@ -0,0 +1,34 @@
+const listings = [
+    {
+        type: 'airline',
+        name: 'British Airways',
+        imageBackground: require('../assets/planes/british_airways.jpg'),
+    },
+    {
+        type: 'airline',
+        name: 'EasyJet',
+        imageBackground: require('../assets/planes/easyjet.jpg'),
+    },
+    {
+        type: 'airline',
+        name: 'Emirates',
+        imageBackground: require('../assets/planes/emirates.png'),
+    },
+    {
+        type: 'airline',
+        name: 'Qatar Airways',
+        imageBackground: require('../assets/planes/qatar_airways.jpg'),
+    },
+    {
+        type: 'airline',
+        name: 'Ryanair',
+        imageBackground: require('../assets/planes/ryanair.jpg'),
+    },
+    {
+        type: 'airline',
+        name: 'Singapore Airlines',
+        imageBackground: require('../assets/planes/singapore_airlines.jpg'),
+    }
+
+]
+export { listings };

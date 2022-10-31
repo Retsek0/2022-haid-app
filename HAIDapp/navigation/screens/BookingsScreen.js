4099934551cfd850d21c50a426f0d4937f2d6d9e
Wenbo-1118 <73350512+Wenbo-1118@users.noreply.github.com>
2022-10-31 13:49:23 +0000

    Add files via upload

---
+++ HAIDapp/navigation/screens/BookingsScreen.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function BookingsScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Bookings Screen</Text>
+        </View>
+    );
+}

900be1767fdd649a76ca5df77cb674fb42aae030
Zhenhao Zhao <cvhm34@durham.ac.uk>
2022-11-07 23:01:06 +0000

    Cost done, except functionality.

--- HAIDapp/Preferences/CostPreference.js
+++ HAIDapp/Preferences/CostPreference.js
@@ -1,5 +1,8 @@
-import { View, Pressable, Text, StyleSheet } from "react-native";
+import { View, Pressable, Text, StyleSheet} from "react-native";
 import { ProgressBar} from 'react-native-paper';
+import { Fontisto } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
 export default function CostPreference(props) {
     return (
@@ -14,9 +17,25 @@ export default function CostPreference(props) {
             <View style={Style.titleContainer}>
                 <Text style={Style.title}>Select your cost preference while travelling </Text>
             </View>
+
+            <Pressable style={Style.fillIn} onPress={() => props.navigation.navigate("StayPreference")}>
+                <Text style={Style.fillInText}>Budget-friendly</Text>
+                <MaterialIcons style={Style.icon} name="money-off" size={50} color="#00716F" />
+            </Pressable>
+
+            <Pressable style={Style.fillIn} onPress={() => props.navigation.navigate("StayPreference")}>
+                <Text style={Style.fillInText} > No Preference </Text>
+                <FontAwesome5 style={Style.icon}  name="money-bill-wave" size={50} color="#00716F" />
+            </Pressable>
+
+            <Pressable style={Style.fillIn} onPress={() => props.navigation.navigate("StayPreference")}>
+                <Text style={Style.fillInText}>Luxury               </Text>
+                <Fontisto style={Style.icon} name="holiday-village" size={45} color="#00716F" />
+            </Pressable>
+
             
-            <Pressable onPress={() => props.navigation.navigate("StayPreference")}>
-                <Text>Next</Text>
+            <Pressable style={Style.continueContainer} onPress={() => props.navigation.navigate("StayPreference")}>
+                <Text style={Style.continueText}>CONTINUE</Text>
             </Pressable>
         </View>
     );
@@ -25,14 +44,13 @@ export default function CostPreference(props) {
 const Style = StyleSheet.create({
     container: {
         alignItems: "center",
-        justifyContent: "center",
         color: "whitesmoke"
     },
 
     progressBar: {
-        height: "13%",
+        height: "10%",
         width: "50%",
-        marginVertical: 25
+        marginTop: "15%",
     },
 
     greetingContainer: {
@@ -55,12 +73,52 @@ const Style = StyleSheet.create({
     },  
 
     title: {
-        fontSize: 42,
+        fontSize: 38,
         fontWeight: "700",
         lineHeight: 50,
         color: "#3F6A84"
-    }
+    },
 
+    fillIn: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignContent: "center",
+        width: "75%",
+        height: "auto",
+        backgroundColor: "white",
+        marginTop: 30,
+        borderColor: "#A5A5A5",
+        borderRadius: 10,
+        borderWidth: 3,
+    },
+
+    fillInText: {
+        fontSize: 25,
+        marginVertical: 30,
+        marginRight: "10%",
+        color: "#909090",
+        fontWeight: "600",
+    },
+
+    icon: {
+       marginVertical: 17,
+       justifyContent: "flex-end",
+    },
+
+    continueContainer: {
+        backgroundColor: "#3F6A84",
+        width: "100%",
+        height: "12%",
+        marginTop: "15%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    continueText: {
+        fontSize: 30,
+        color: "white",
+        fontWeight: "600"
+    }
 
 
 });
